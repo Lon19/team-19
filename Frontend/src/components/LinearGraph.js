529aecf9ba60988dc8cecff29f93a16f5221612f
Adam Marian Wangrat <adam.wangrat@neuralalpha.com>
2019-11-02 08:55:09 +0000

    prepare plots for interplot connection

--- Frontend/src/components/LinearGraph.js
+++ Frontend/src/components/LinearGraph.js
@@ -1,61 +1,60 @@
-import React, { Component } from 'react'
-import { VictoryChart, VictoryScatter, VictoryLine, VictoryPolarAxis, VictoryGroup, VictoryArea } from 'victory';
+import React, { Component } from "react";
+import {
+  VictoryChart,
+  VictoryScatter,
+  VictoryLine,
+  VictoryPolarAxis,
+  VictoryGroup,
+  VictoryArea
+} from "victory";
 
 const myBlue = "#75b2ff";
 
 class LinearGraph extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            ready: false,
-            selection: "pressure"
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      ready: false,
+      selection: "pressure"
+    };
+  }
 
-    this.processData.bind(this);
-    }
+  componentDidMount() {
+    fetch(
+      "http://127.0.0.1:5000/work-self-confidence/overview?username=67611589",
+      {
+        mode: "cors"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          data: data.map(a => {
+            return { x: new Date(a.date), y: a[this.props.selection] };
+          }),
+          ready: true
+        });
+      });
+  }
 
-    componentDidMount() {
-        fetch("http://127.0.0.1:5000/work-self-confidence/overview?username=67611589", {
-            mode: 'cors'
-        })
-            .then(response => {
-                    return response.json()
-                }
-            )
-            .then(data => {
-                this.setState({data: data.map(a => {
-                        return {date: new Date(a.date), item: a[this.state.selection]};
-                    }), ready: true });
-            });
+  render() {
+    if (!this.state.ready) {
+      return null;
     }
-     processData (data)  {
-        const makeDataArray = (d) => {
-            return Object.keys(d).map((key) => {
-                return { x: new Date(d.date), y: d.item };
-            });
-        };
-        return data.map((datum) => makeDataArray(datum));
-    }
-
-    render() {
-         if (!this.state.ready) {
-             return null;
-         }
-         console.log(this.state.data);
-        return (
-            <VictoryChart
-                scale={{ x: "time" }}
-            >
-                <VictoryLine
-                    style={{
-                        data: { stroke: "#c43a31" },
-                        parent: { border: "1px solid #ccc"}
-                    }}
-                    data={this.state.date}
-                />
-            </VictoryChart>
-        );
-    }
+    return (
+      <VictoryChart scale={{ x: "time" }}>
+        <VictoryLine
+          style={{
+            data: { stroke: "#c43a31" },
+            parent: { border: "1px solid #ccc" }
+          }}
+          data={this.state.data}
+        />
+      </VictoryChart>
+    );
+  }
 }
-export default LinearGraph
+export default LinearGraph;
